ff04de5e0874cb7aca9ef834ddc4a610b1b2fc14
tuanliz0403 <97226212+tuanliz0403@users.noreply.github.com>
2024-09-09 08:52:40 +0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define cen cout << "\n";
+const int mxn = 2e5 + 7;
+ 
+using namespace std;
+typedef pair<int,int> pii;
+ 
+struct customsort{
+    bool operator()(int a, int b) const{
+        return a > b;
+    }
+};
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int len, m;
+    cin >> len >> m;
+    return 0;
+}
