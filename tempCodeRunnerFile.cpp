70123eb22c945b01247b870dba494892609be5aa
tuanliz0403 <97226212+tuanliz0403@users.noreply.github.com>
2024-09-19 16:28:14 +0700

    xoa file temp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-    // for(int i = 0; i < n; i++)
-    // {
-    //     for(int j = 0; j < m; j++)
-    //     {
-    //         cout << g[i][j] <<  setw(5);
-    //     }
-    //     cen;
-    // }
