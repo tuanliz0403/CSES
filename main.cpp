95c8fcfffb3994dc2c4e3b004d5db29f375bc62b
tuanliz0403 <97226212+tuanliz0403@users.noreply.github.com>
2024-09-22 16:45:16 +0700

    22/9/2024

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
-#define cen cout << endl;
 using namespace std;
  
-signed main(){
-    int n;
+int main(){
+    unsigned long long n;
     cin >> n;
+    while (true){
+        cout << n << " ";
+        if(n == 1) return 0;
+        if(n & 1) n = n * 3 + 1;
+        else n /= 2;
+    }
+    return 0;
 }
