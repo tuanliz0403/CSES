5d9f60d3e5a77cf252426629baf33dae7908b41a
tuanliz0403 <97226212+tuanliz0403@users.noreply.github.com>
2024-09-22 18:31:34 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
-    unsigned long long n;
-    cin >> n;
-    while (true){
-        cout << n << " ";
-        if(n == 1) return 0;
-        if(n & 1) n = n * 3 + 1;
-        else n /= 2;
-    }
+
+int main()
+{
+    int a = 3, b = 4; int c = a * a + b * b; cout << c; 
     return 0;
 }
