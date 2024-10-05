9b3dbd8a5dcf8b522fa6ae628f50ac132b3f25b4
tuanliz0403 <97226212+tuanliz0403@users.noreply.github.com>
2024-10-05 16:25:55 +0700

    5-10-2024

--- main.cpp
+++ main.cpp
@@ -1,57 +1,7 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define cen cout << "\n";
-#define space << " "
-#define f0(n) for (ll i = 0; i < n; i++)
-#define fo(a, b) for (ll i = a; i != b; i++)
-#define pb push_back
-#define se second
-#define fi first
-#define mp(x, y) make_pair(x, y)
-#define vec vector
-#define ins insert
-#define begin begin()
-#define end end()
 using namespace std;
-const ll MOD = 1e9 + 7;
-const ll mxn = 1e6 + 7;
-const ll inf = 1e9;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
 
-/******************************************MY VARIABLES******************************************/
-int n, k;
-/******************************************MY FUNCTIONS******************************************/
-class Solution
+int main()
 {
-public:
-  int minSubarray(vector<int> &num, int p)
-  {
-    int n = num.size();
-    vector<long long> pre(n + 1, 0);
-    int current = 0;
-    for (int i = 0; i < n; i++)
-    {
-      current += num[i];
-      pre[i + 1] = current;
-    }
-    int rem = current % p;
-    for (int i : pre)
-      cout << i space;
-    return 0;
-  }
-};
-signed main()
-{
-  ios_base::sync_with_stdio(NULL);
-  cin.tie(0);
-  ifstream cin("file.inp");
-  // ofstream cout("file.out");
-  vector<int> arr;
-  int temp;
-  int p = 6;
-  while (cin >> temp)
-    arr.push_back(temp);
-  Solution solve;
-  int ans = solve.minSubarray(arr, p);
+  
 }
